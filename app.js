5444f0f29466756e0f4c3dd8a65f4497b7f22e2e
unknown <iarafat386@gmail.com>
2021-07-30 14:53:17 +0600

    Project Started

---
+++ app.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question: 'How old is me',
+        a_1: '10',
+        b: '17',
+        c: '26',
+        d: '110',
+        correct: 'c'
+    }, {
+        question: 'What is the most used PL in 2021',
+        a: 'Java',
+        b: 'C',
+        c: 'Golang',
+        d: 'JavaScript',
+        correct :'d'
+    }, {
+        question: 'Who is the president of US',
+        a: 'Chonda',
+        b: 'Mannan',
+        c: 'Asma',
+        d: 'Nasima',
+        correct :'a'
+    }, {
+        question:'What does HTML stands for',
+        a: 'Hypertext Markup Language',
+        b: 'Cascading Style Sheet',
+        c: 'Jason Object Notation',
+        d: 'Heli Tall Man Loves',
+        correct: 'a'
+    }, {
+        question:'What year was JavaScript launched',
+        a: '1996',
+        b: '1995',
+        c: '1994',
+        d: 'None of the avobe',
+        correct: 'd'
+    }
+]
